7dfa6b01e9046c08607d854c96ac70d7ab6fe6e4
Jangwook Ryu <rxjw95@gmail.com>
2022-03-12 17:18:19 +0900

    [chapter2] 변수와 자료형, 연산자
    - smart casting (Number, Any)

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,11 @@
 fun main() {
-    val str: String? = null
-    print(str?.length) // null
-    // print(str!!.length) // NPE
+    var any:Any = 12.2f // 스마트 캐스팅
+
+    if(any is Int) {
+        print("Int")
+    } else if(any is Float) {
+        print("Float") // 해당 분기를 탄다.
+    } else if(any is Double) {
+        print("Double")
+    }
 }
